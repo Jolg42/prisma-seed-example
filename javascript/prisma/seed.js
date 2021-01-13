30d2df18e5d5736e4c14539a869c3bc5693f8a6b
Joël Galeran <Jolg42@users.noreply.github.com>
2021-01-13 14:02:08 +0100

    add random

--- javascript/prisma/seed.js
+++ javascript/prisma/seed.js
@@ -1,52 +1,55 @@
-const dotenv = require("dotenv");
-const { PrismaClient } = require("@prisma/client");
+const dotenv = require("dotenv")
+const { PrismaClient } = require("@prisma/client")
 
-dotenv.config();
-const prisma = new PrismaClient();
+dotenv.config()
+const prisma = new PrismaClient()
 
 async function main() {
+  // To satisfy the unique constraint on email if running the seed multiple times
+  const random = Math.random().toString(36).substring(7)
+  
   const alice = await prisma.user.create({
     data: {
-      email: "alice@prisma.io",
-      name: "Alice",
+      email: `alice${random}@prisma.io`,
+      name: 'Alice',
       posts: {
         create: {
-          title: "Check out Prisma with Next.js",
-          content: "https://www.prisma.io/nextjs",
+          title: 'Check out Prisma with Next.js',
+          content: 'https://www.prisma.io/nextjs',
           published: true,
         },
       },
     },
-  });
+  })
 
   const bob = await prisma.user.create({
     data: {
-      email: "bob@prisma.io",
-      name: "Bob",
+      email: `bob${random}@prisma.io`,
+      name: 'Bob',
       posts: {
         create: [
           {
-            title: "Follow Prisma on Twitter",
-            content: "https://twitter.com/prisma",
+            title: 'Follow Prisma on Twitter',
+            content: 'https://twitter.com/prisma',
             published: true,
           },
           {
-            title: "Follow Nexus on Twitter",
-            content: "https://twitter.com/nexusgql",
+            title: 'Follow Nexus on Twitter',
+            content: 'https://twitter.com/nexusgql',
             published: true,
           },
         ],
       },
     },
-  });
-  console.log({ alice, bob });
+  })
+  console.log({ alice, bob })
 }
 
 main()
   .catch((e) => {
-    console.error(e);
-    process.exit(1);
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })
